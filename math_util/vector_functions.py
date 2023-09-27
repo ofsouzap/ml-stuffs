NN layer multi functions and fixed backwards grad formula

math_util/vector_functions.py
@@ -1,44 +1,162 @@
-from typing import NamedTuple, Callable
+from typing import Callable
 import numpy as np
 import numpy.typing as npt
 
 
-class DiffVectorVectorFunction(NamedTuple):
-    """A function operating on and returning a vector that is differentiable"""
-    f: Callable[[npt.NDArray], npt.NDArray]
-    grad_f: Callable[[npt.NDArray], npt.NDArray]
+class DiffVectorVectorFunction:
+    """An immutable differentiable function operating on and returning a vector"""
 
+    def __init__(self,
+                 f_multi: Callable[[npt.NDArray], npt.NDArray],
+                 grad_f_multi: Callable[[npt.NDArray], npt.NDArray]):
+        self.__f_multi = f_multi
+        self.__grad_f_multi = grad_f_multi
 
-class NNCostFunction(NamedTuple):
-    """A differentiable cost function for a neural network"""
-
-    f: Callable[[npt.NDArray, npt.NDArray], float]
-    """The cost function.
+    def f(self, x: npt.NDArray) -> npt.NDArray:
+        """Apply the function to a single input.
 
 Parameters:
 
-    observed - the observed output vector
+    x - the input vector
 
-    expected - the expected output vector
+Returns:
+
+    y - the output vector
+"""
+        assert x.ndim == 1, "Input should be one-dimensional"
+
+        return self.f_multi(x[np.newaxis,:])[0]
+
+    def f_multi(self, xs: npt.NDArray) -> npt.NDArray:
+        """Apply the function to multiple inputs.
+
+Parameters:
+
+    xs - the input vectors
+
+Returns:
+
+    ys - the output vectors
+"""
+        assert xs.ndim == 2, "Input should be two-dimensional"
+
+        return self.__f_multi(xs)
+
+    def grad_f(self, x: npt.NDArray) -> npt.NDArray:
+        """Apply the gradient function to a single input.
+
+Parameters:
+
+    x - the input vector
+
+Returns:
+
+    y - the output vector
+"""
+        assert x.ndim == 1, "Input should be one-dimensional"
+
+        return self.grad_f_multi(x[np.newaxis,:])[0]
+
+    def grad_f_multi(self, xs: npt.NDArray) -> npt.NDArray:
+        """Apply the gradient function to multiple inputs.
+
+Parameters:
+
+    xs - the input vectors
+
+Returns:
+
+    ys - the output vectors
+"""
+        assert xs.ndim == 2, "Input should be two-dimensional"
+
+        return self.__grad_f_multi(xs)
+
+
+class NNCostFunction:
+    """An immutable differentiable cost function for a neural network"""
+
+    def __init__(self,
+                 f_multi: Callable[[npt.NDArray, npt.NDArray], npt.NDArray],
+                 grad_f_multi: Callable[[npt.NDArray, npt.NDArray], npt.NDArray]):
+        self.__f_multi = f_multi
+        self.__grad_f_multi = grad_f_multi
+
+    def f(self, obs: npt.NDArray, exp: npt.NDArray) -> float:
+        """Apply the cost function.
+
+Parameters:
+
+    obs - the observed output vector
+
+    exp - the expected output vector
 
 Returns:
 
     cost - the calculated cost of the observation
 """
+        assert obs.ndim == exp.ndim == 1, "Inputs must be one-dimensional"
 
-    grad_f: Callable[[npt.NDArray, npt.NDArray], npt.NDArray]
-    """The derivative function of the cost function w.r.t. the observed output vector (i.e. ∂ cost / ∂ observed_value).
+        return self.__f_multi(
+            obs[np.newaxis,:],
+            exp[np.newaxis,:],
+        )[0]
+
+    def f_multi(self, obss: npt.NDArray, exps: npt.NDArray) -> npt.NDArray:
+        """Apply the cost function to multiple inputs.
 
 Parameters:
 
-    observed - the observed output vector
+    obss - the observed output vectors
 
-    expected - the expected output vector
+    exps - the expected output vectors
+
+Returns:
+
+    costs - a vector of the calculated costs of the observation
+"""
+        assert obss.ndim == exps.ndim == 2, "Inputs must be two-dimensional"
+        assert obss.shape[0] == exps.shape[0], "Inputs must have the same number of values"
+
+        return self.__f_multi(obss, exps)
+
+    def grad_f(self, obs: npt.NDArray, exp: npt.NDArray) -> npt.NDArray:
+        """Apply the derivative function of the cost function w.r.t. the observed output vector (i.e. ∂ cost / ∂ observed_value).
+
+Parameters:
+
+    obs - the observed output vector
+
+    exp - the expected output vector
 
 Returns:
 
     grad - the calculated gradient of the cost of the observation with respect to the observation
 """
+        assert obs.ndim == exp.ndim == 1, "Inputs must be one-dimensional"
+
+        return self.grad_f_multi(
+            obs[np.newaxis,:],
+            exp[np.newaxis,:],
+        )[0]
+
+    def grad_f_multi(self, obss: npt.NDArray, exps: npt.NDArray) -> npt.NDArray:
+        """Apply the derivative function of the cost function w.r.t. the observed output vector (i.e. ∂ cost / ∂ observed_value) to apply to multiple inputs.
+
+Parameters:
+
+    observed - the observed output vectors
+
+    expected - the expected output vectors
+
+Returns:
+
+    grads - the calculated gradients of the cost of the observation with respect to the observations
+"""
+        assert obss.ndim == exps.ndim == 2, "Inputs must be two-dimensional"
+        assert obss.shape[0] == exps.shape[0], "Inputs must have same number of values"
+
+        return self.__grad_f_multi(obss, exps)
 
 
 relu = DiffVectorVectorFunction(
@@ -54,6 +172,6 @@ sigmoid = DiffVectorVectorFunction(
 
 
 sum_of_squares_cost = NNCostFunction(
-    lambda obs, exp: np.sum(np.square(obs - exp), axis=0),
-    lambda obs, exp: 2 * (obs - exp),
+    f_multi=lambda obss, exps: np.sum(np.square(obss - exps), axis=1),
+    grad_f_multi=lambda obss, exps: 2 * (obss - exps),
 )

neural_networks/layers.py
@@ -36,24 +36,32 @@ class LayerBase(ABC):
     def __repr__(self) -> str:
         return str(self)
 
-    @abstractmethod
-    def forwards(self, x: npt.NDArray) -> npt.NDArray:
-        """Performs forwards-propagation"""
-        pass
+    def forwards_single(self, x: npt.NDArray) -> npt.NDArray:
+        """Performs forwards-propagation on a single input"""
+        return self.forwards_multi(x[np.newaxis,:])[0]
 
     @abstractmethod
-    def backwards(self, x: npt.NDArray, grad_wrt_y: npt.NDArray) -> npt.NDArray:
-        """Performs backwards-propagation, altering the parameters of the layer according to the learning rate.
+    def forwards_multi(self, xs: npt.NDArray) -> npt.NDArray:
+        """Performs forwards-propagation on multiple inputs"""
+        pass
+
+    def backwards_single(self, x: npt.NDArray, grad_wrt_y: npt.NDArray) -> npt.NDArray:
+        """Performs backwards-propagation but only on a single input"""
+        return self.backwards_multi(x[np.newaxis,:], grad_wrt_y[np.newaxis,:])[0]
+
+    @abstractmethod
+    def backwards_multi(self, x: npt.NDArray, grads_wrt_ys: npt.NDArray) -> npt.NDArray:
+        """Performs backwards-propagation on multiple inputs, altering the parameters of the layer according to the learning rate.
 
 Parameters:
 
-    x - the inputs that were passed to the layer
+    xs - the input vectors that were passed to the layer
 
-    grad_wrt_y - the gradient vector of the cost function with respect to the outputs of the function.
+    grads_wrt_ys - the gradient vectors of the cost function with respect to the outputs of the function.
 
 Returns:
 
-    grad_wrt_x - the gradient vector of the cost function with respect to the inputs of the function.
+    grad_wrt_xs - the gradient vectors of the cost function with respect to the inputs of the function.
 """
         pass
 
@@ -73,11 +81,14 @@ class ActivationLayer(LayerBase):
     def func(self) -> DiffVectorVectorFunction:
         return self._func
 
-    def forwards(self, x: npt.NDArray) -> npt.NDArray:
-        return self.func.f(x)
+    def forwards_multi(self, xs: npt.NDArray) -> npt.NDArray:
+        assert xs.ndim == 2, "Input must be two-dimensional"
+        return self.func.f_multi(xs)
 
-    def backwards(self, x: npt.NDArray, grad_wrt_y: npt.NDArray) -> npt.NDArray:
-        return np.multiply(grad_wrt_y, self.func.grad_f(x))
+    def backwards_multi(self, xs: npt.NDArray, grads_wrt_ys: npt.NDArray) -> npt.NDArray:
+        assert xs.ndim == grads_wrt_ys.ndim == 2, "Inputs must be two-dimensional"
+        assert xs.shape[0] == grads_wrt_ys.shape[0], "Inputs must have same number of values"
+        return grads_wrt_ys * self.func.grad_f_multi(xs)
 
 
 class ReluActivationLayer(ActivationLayer):
@@ -142,49 +153,69 @@ class DenseLayer(LayerBase):
             )
         return " & ".join([f"({s})" for s in output_form_strs])
 
-    def forwards(self, x: npt.NDArray) -> npt.NDArray:
-        assert x.shape == (self.input_n,), "Input shape invalid"
-        return (x @ self.weights) + self.bias
+    def forwards_multi(self, xs: npt.NDArray) -> npt.NDArray:
+        assert xs.ndim == 2, "Input must be two-dimensional"
 
-    def backwards(self, x: npt.NDArray, grad_wrt_y: npt.NDArray) -> npt.NDArray:
+        mat_xs = xs[:,np.newaxis,:]
+        mat_result: npt.NDArray = np.matmul(mat_xs, self.weights) + self.bias
+        result = mat_result[:,0,:]  # The results of the matrix multiplication was an array of row vectors, this just takes each row vector as a 1D array
 
-        assert x.shape == (self.input_n,), "Invalid x shape"
-        assert grad_wrt_y.shape == (self.output_n,), "Invalid grad_wrt_y shape"
+        assert result.ndim == 2
+        assert result.shape[0] == xs.shape[0]
+
+        return result
+
+    def backwards_multi(self, xs: npt.NDArray, grads_wrt_ys: npt.NDArray) -> npt.NDArray:
+        assert xs.ndim == grads_wrt_ys.ndim == 2, "Inputs must be two-dimensional"
+        assert xs.shape[0] == grads_wrt_ys.shape[0], "Inputs must have same number of values"
+        assert xs.shape[1] == self.input_n, "xs should have same number of values as layer has inputs"
+        assert grads_wrt_ys.shape[1] == self.output_n, "grads_wrt_ys should have same number of values as layer has outputs"
+
+        # Calculate gradients w.r.t. inputs (i.e. ∂ cost / ∂ x) before changing parameter values
+
+        grads_wrt_xs: npt.NDArray = np.sum(grads_wrt_ys[:,np.newaxis,:] * self.weights[np.newaxis,:,:], axis=2)
+        """∂ cost_i / ∂ x_j = Σ_k ( ∂ cost_i / ∂ y_k ) * W_jk"""
 
         # Calculate gradients w.r.t. parameters
 
-        grad_wrt_weights = x[:,np.newaxis] * grad_wrt_y[np.newaxis,:]
-        """∂ cost / ∂ W_ij = x_i * ∂ cost / ∂ y_j"""
+        grads_wrt_weights = xs[:,:,np.newaxis] * grads_wrt_ys[:,np.newaxis,:]
+        """∂ cost_i / ∂ W_jk = x_j * ∂ cost_i / ∂ y_k"""
 
-        grad_wrt_biases = grad_wrt_y.copy()
-        """∂ cost / ∂ b_i = ∂ cost / ∂ y_i"""
+        grads_wrt_biases = grads_wrt_ys.copy()
+        """∂ cost_i / ∂ b_j = ∂ cost_i / ∂ y_j"""
 
         # Apply gradient descent
 
-        self.add_weights(-1 * self.learning_rate * grad_wrt_weights)
-        self.add_biases(-1 * self.learning_rate * grad_wrt_biases)
+        self._add_weights_multi(-1 * self.learning_rate * grads_wrt_weights)
+        self._add_biases_multi(-1 * self.learning_rate * grads_wrt_biases)
 
-        # Calculate gradients w.r.t. inputs (i.e. ∂ cost / ∂ x)
+        # Check output
 
-        grad_wrt_x = np.sum(grad_wrt_weights, axis=1)
-        """∂ cost / ∂ x_i = Σ_j ( ∂ cost / ∂ W_ij )"""
+        assert grads_wrt_xs.ndim == 2
+        assert grads_wrt_xs.shape[0] == xs.shape[0]
+        assert grads_wrt_xs.shape[1] == xs.shape[1]
 
-        return grad_wrt_x
+        # Return output
+
+        return grads_wrt_xs
 
     def set_weights(self, weights: npt.NDArray) -> None:
         assert weights.shape == (self.input_n,self.output_n), "Invalid weight matrix shape"
         self._weights = weights
 
-    def add_weights(self, add_weights: npt.NDArray) -> None:
-        """Adds the specified amount to the weights"""
-        assert add_weights.shape == self.weights.shape, "Invalid add_weights shape"
-        self._weights += add_weights
+    def _add_weights_multi(self, add_weights: npt.NDArray) -> None:
+        """Adds the specified amounts to the weights"""
+        assert add_weights.ndim == 3, "Input must be three-dimensional"
+        assert add_weights.shape[1] == self.weights.shape[0], "Incorrect add weights shape"
+        assert add_weights.shape[2] == self.weights.shape[1], "Incorrect add weights shape"
+        self._weights += np.sum(add_weights, axis=0)
 
     def set_bias(self, bias: npt.NDArray) -> None:
         assert bias.shape == (self.output_n,), "Invalid bias array shape"
         self._bias = bias
 
-    def add_biases(self, add_biases: npt.NDArray) -> None:
-        """Adds the specified amount to the biases"""
-        assert add_biases.shape == self.bias.shape, "Invalid add_biases shape"
-        self._bias += add_biases
+    def _add_biases_multi(self, add_biases: npt.NDArray) -> None:
+        """Adds the specified amounts to the biases"""
+        assert add_biases.ndim == 2, "Input must be two-dimensional"
+        assert add_biases.shape[1] == self.bias.shape[0], "Incorrect number of bias values"
+        self._bias += np.sum(add_biases, axis=0)

tests/unit/neural_networks/test_dense_layer.py
@@ -9,35 +9,47 @@ from neural_networks.layers import DenseLayer
 DEFAULT_LEARNING_RATE = 1e-3
 
 
-_IDENTITIY_CASES: Iterable[Tuple[int, npt.NDArray]] = [
+_IDENTITIY_CASES_SINGLE: Iterable[Tuple[npt.NDArray]] = [
     (
-        2,
-        np.array([ 5, 3 ], dtype=np.float64)
+        np.array([ 5, 3 ], dtype=np.float64),
     ),
     (
-        3,
-        np.array([ -4, 3, 6.4 ], dtype=np.float64)
+        np.array([ -4, 3, 6.4 ], dtype=np.float64),
     ),
     (
-        4,
-        np.array([ 5.3, 6.0, -1.0, 0.0 ], dtype=np.float64)
+        np.array([ 5.3, 6.0, -1.0, 0.0 ], dtype=np.float64),
     ),
     (
-        4,
-        np.array([ -1.0, -2.1, 9.9, 2.4 ], dtype=np.float64)
+        np.array([ -1.0, -2.1, 9.9, 2.4 ], dtype=np.float64),
     ),
     (
-        6,
-        np.array([ 0.0, 0.3, 0.0, 0.0, 5.6, 2.3 ], dtype=np.float64)
+        np.array([ 0.0, 0.3, 0.0, 0.0, 5.6, 2.3 ], dtype=np.float64),
     ),
     (
-        6,
-        np.array([ 10.4, -1742, 94, -0.5316, 256.3, 0.1 ], dtype=np.float64)
+        np.array([ 10.4, -1742, 94, -0.5316, 256.3, 0.1 ], dtype=np.float64),
     ),
 ]
 
 
-_FORWARDS_CASES_AUTO_CALC: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArray]] = [
+_IDENTITIY_CASES_MULTI: Iterable[Tuple[npt.NDArray]] = [
+    (
+        np.array([
+            [ 2, 4.0, 0.0 ],
+            [ -2, 6.0, 3.0 ],
+            [ -2, 1.3, 0.3 ],
+        ], dtype=np.float64),
+    ),
+    (
+        np.array([
+            [ 2, 4.0, 0.0, 0.0 ],
+            [ -2, 6.0, 3.0, 1.2 ],
+            [ -2, 1.3, 0.3, 1.2 ],
+        ], dtype=np.float64),
+    ),
+]
+
+
+_FORWARDS_CASES_AUTO_CALC_SINGLE: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArray]] = [
     (
         np.array([
             [ 1, 2 ],
@@ -95,7 +107,66 @@ _FORWARDS_CASES_AUTO_CALC: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArray]
 ]
 
 
-_BACKWARDS_CASES_CHANGE_DIRS: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray[np.bool_], npt.NDArray[np.bool_]]] = [
+_FORWARDS_CASES_AUTO_CALC_MULTI: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArray]] = [
+    (
+        np.array([
+            [ 1, 2 ],
+            [ 3, 4 ],
+            [ 5, 6 ],
+        ], dtype=np.float64),
+        np.zeros(shape=(2,), dtype=np.float64),
+        np.array([
+            [ 1, 1, 1 ],
+            [ 0, 2, 1.2 ],
+            [ 0, 4, 3 ],
+        ], dtype=np.float64),
+    ),
+    (
+        np.array([
+            [ 1, 2 ],
+            [ 3, 4 ],
+            [ 5, 6 ],
+        ], dtype=np.float64),
+        np.zeros(shape=(2,), dtype=np.float64),
+        np.array([
+            [ 1, -6, 3.2 ],
+        ], dtype=np.float64),
+    ),
+    (
+        np.array([
+            [ 1, 2 ],
+            [ 3, 4 ],
+            [ 5, 6 ],
+        ], dtype=np.float64),
+        np.array([ 0.1, -0.1 ], dtype=np.float64),
+        np.array([
+            [ 1, -6, 3.2 ],
+            [ 0, 0, 0 ],
+            [ 7.5, 3.1, -100 ],
+            [ 1, 1, 1 ],
+        ], dtype=np.float64),
+    ),
+    (
+        np.array([
+            [ 1, 2, -2.4, 0.5 ],
+            [ 3, 4.1, 1, 2 ],
+            [ 5, 6, -6, 3 ],
+        ], dtype=np.float64),
+        np.array([ 0.1, 0.2, -0.3, 10.2 ], dtype=np.float64),
+        np.array([
+            [ -1.0, 1.24, 0.4 ],
+            [ 0, 0, 0 ],
+            [ 1, 1, 1 ],
+            [ 2, 2, 2 ],
+            [ -100, 100, 100 ],
+            [ 200, 200, 5 ],
+            [ 3.14, 1.59, 2.63 ],
+        ], dtype=np.float64),
+    ),
+]
+
+
+_BACKWARDS_CASES_CHANGE_DIRS_SINGLE: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray[np.bool_], npt.NDArray[np.bool_]]] = [
     (
         np.array([
             [ 1 ],
@@ -137,7 +208,69 @@ _BACKWARDS_CASES_CHANGE_DIRS: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArr
 ]
 
 
-_BACKWARDS_CASES_OUTPUT_GRADS: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]] = [
+_BACKWARDS_CASES_CHANGE_DIRS_MULTI: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray[np.bool_], npt.NDArray[np.bool_]]] = [
+    (
+        np.array([
+            [ 1 ],
+            [ 1 ],
+        ], dtype=np.float64),
+        np.zeros(shape=(1,), dtype=np.float64),
+        np.array([
+            [ 1, 1 ],
+            [ 2, 1 ],
+        ], dtype=np.float64),
+        np.array([
+            [ 1 ],
+            [ 1 ],
+        ], dtype=np.float64),
+        np.zeros(shape=(2,1), dtype=np.bool_),
+        np.zeros(shape=(1,), dtype=np.bool_),
+    ),
+    (
+        np.array([
+            [ 1 ],
+            [ 1 ],
+        ], dtype=np.float64),
+        np.zeros(shape=(1,), dtype=np.float64),
+        np.array([
+            [ 1, 1 ],
+            [ 3, 5 ],
+        ], dtype=np.float64),
+        np.array([
+            [ -1 ],
+            [ -1 ],
+        ], dtype=np.float64),
+        np.ones(shape=(2,1), dtype=np.bool_),
+        np.ones(shape=(1,), dtype=np.bool_),
+    ),
+    (
+        np.array([
+            [ 3, -3 ],
+            [ 4, 0 ],
+            [ 1, 1 ],
+        ], dtype=np.float64),
+        np.array([ 0, 2 ], dtype=np.float64),
+        np.array([
+            [ 1, -2, 10 ],
+            [ 0, 0, 10 ],
+            [ 1, -5, 10 ],
+        ], dtype=np.float64),
+        np.array([
+            [ 1, -3.2 ],
+            [ 1, -3.2 ],
+            [ 1, -3.2 ],
+        ], np.float64),
+        np.array([
+            [ False, True ],
+            [ True, False ],
+            [ False, True ],
+        ], dtype=np.bool_),
+        np.array([ False, True ], dtype=np.bool_),
+    ),
+]
+
+
+_BACKWARDS_CASES_OUTPUT_GRADS_SINGLE: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]] = [
     (
         np.array([
             [ 1 ],
@@ -167,7 +300,7 @@ _BACKWARDS_CASES_OUTPUT_GRADS: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDAr
         np.array([ 0, 2 ], dtype=np.float64),
         np.array([ 1, -2, 10 ], dtype=np.float64),
         np.array([ 1, -3.2 ], np.float64),
-        np.array([ -2.2, 4.4, -22 ], np.float64),
+        np.array([ 12.6, 4, -2.2 ], np.float64),
     ),
     (
         np.array([
@@ -182,6 +315,52 @@ _BACKWARDS_CASES_OUTPUT_GRADS: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDAr
 ]
 
 
+_BACKWARDS_CASES_OUTPUT_GRADS_MULTI: Iterable[Tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]] = [
+    (
+        np.array([
+            [ 1 ],
+            [ 1 ],
+        ], dtype=np.float64),
+        np.zeros(shape=(1,), dtype=np.float64),
+        np.array([
+            [ 1, 1 ],
+            [ 0, 0 ],
+            [ 1, -1 ],
+        ], dtype=np.float64),
+        np.array([
+            [ 1 ],
+            [ 1 ],
+            [ 1 ],
+        ], dtype=np.float64),
+        np.array([
+            [ 1, 1 ],
+            [ 1, 1 ],
+            [ 1, 1 ],
+        ], np.float64),
+    ),
+    (
+        np.array([
+            [ 3, -3 ],
+            [ 4, 0 ],
+            [ 1, 1 ],
+        ], dtype=np.float64),
+        np.array([ 0, 2 ], dtype=np.float64),
+        np.array([
+            [ 1, -2, 10 ],
+            [ 0, 3, 14 ],
+        ], np.float64),
+        np.array([
+            [ 1, -3.2 ],
+            [ 1, -3.2 ],
+        ], np.float64),
+        np.array([
+            [ 12.6, 4, -2.2 ],
+            [ 12.6, 4, -2.2 ],
+        ], np.float64),
+    ),
+]
+
+
 def _create(w: npt.NDArray, b: npt.NDArray, learning_rate: float = DEFAULT_LEARNING_RATE) -> DenseLayer:
     return DenseLayer(
         n=w.shape[0],
@@ -192,7 +371,7 @@ def _create(w: npt.NDArray, b: npt.NDArray, learning_rate: float = DEFAULT_LEARN
     )
 
 
-def _calc_auto_exp_forwards(w: npt.NDArray, b: npt.NDArray, inp: npt.NDArray) -> npt.NDArray:
+def _calc_auto_exp_forwards_single(w: npt.NDArray, b: npt.NDArray, inp: npt.NDArray) -> npt.NDArray:
     assert inp.ndim == b.ndim == 1
     assert w.ndim == 2
     assert w.shape[0] == inp.shape[0]
@@ -221,7 +400,37 @@ def _calc_auto_exp_forwards(w: npt.NDArray, b: npt.NDArray, inp: npt.NDArray) ->
     return out
 
 
-def _run_test_forwards(w: npt.NDArray, b: npt.NDArray, inp: npt.NDArray, exp: npt.NDArray):
+def _calc_auto_exp_forwards_multi(w: npt.NDArray, b: npt.NDArray, inps: npt.NDArray) -> npt.NDArray:
+    assert inps.ndim == 2
+    assert w.ndim == 2
+    assert b.ndim == 1
+    assert w.shape[0] == inps.shape[1]
+    assert w.shape[1] == b.shape[0]
+
+    N, M = w.shape[0], w.shape[1]
+
+    out = np.zeros(shape=(inps.shape[0],w.shape[1],))
+
+    # Weights
+
+    for j in range(M):
+
+        out[:,j] = 0  # Just being safe, however unnecessary
+
+        for i in range(N):
+
+            add_vals = (inps[:,i] * w[i,j])
+            out[:,j] += add_vals
+
+    # Biases
+
+    for j in range(M):
+        out[:,j] += b[j]
+
+    return out
+
+
+def _run_test_forwards_single(w: npt.NDArray, b: npt.NDArray, inp: npt.NDArray, exp: npt.NDArray):
     assert w.ndim == 2
     assert b.ndim == inp.ndim == exp.ndim == 1
     assert w.shape[0] == inp.shape[0]
@@ -231,28 +440,60 @@ def _run_test_forwards(w: npt.NDArray, b: npt.NDArray, inp: npt.NDArray, exp: np
     layer = _create(w, b)
 
     # Act
-    out = layer.forwards(inp)
+    out = layer.forwards_single(inp)
 
     # Assert
     assert_allclose(out, exp)
 
 
-@pytest.mark.parametrize(["N", "inp"], _IDENTITIY_CASES)
-def test_forwards_identity(N: int, inp: npt.NDArray):
-    assert inp.shape == (N,)
+def _run_test_forwards_multi(w: npt.NDArray, b: npt.NDArray, inps: npt.NDArray, exps: npt.NDArray):
+    assert w.ndim == 2
+    assert b.ndim == 1
+    assert inps.ndim == exps.ndim == 2
+    assert w.shape[0] == inps.shape[1]
+    assert w.shape[1] == b.shape[0] == exps.shape[1]
+
+    # Arrange
+    layer = _create(w, b)
+
+    # Act
+    outs = layer.forwards_multi(inps)
+
+    # Assert
+    assert_allclose(outs, exps)
+
+
+@pytest.mark.parametrize(["inp"], _IDENTITIY_CASES_SINGLE)
+def test_forwards_identity_single(inp: npt.NDArray):
+    N = inp.shape[0]
     w = np.identity(N)
     b = np.zeros(shape=N)
-    return _run_test_forwards(w, b, inp, inp)
+    return _run_test_forwards_single(w, b, inp, inp)
 
 
-@pytest.mark.parametrize(["w", "b", "inp"], _FORWARDS_CASES_AUTO_CALC)
-def test_forwards_cases_auto_calc(w, b, inp):
-    exp = _calc_auto_exp_forwards(w, b, inp)
-    return _run_test_forwards(w, b, inp, exp)
+@pytest.mark.parametrize(["inps"], _IDENTITIY_CASES_MULTI)
+def test_forwards_identity_multi(inps: npt.NDArray):
+    assert inps.ndim == 2
+    N = inps.shape[1]
+    w = np.identity(N)
+    b = np.zeros(shape=N)
+    return _run_test_forwards_multi(w, b, inps, inps)
 
 
-@pytest.mark.parametrize(["w", "b", "xs", "grad_wrt_ys", "exp_ws_inc", "exp_bs_inc"], _BACKWARDS_CASES_CHANGE_DIRS)
-def test_backwards_cases_change_directions(
+@pytest.mark.parametrize(["w", "b", "inp"], _FORWARDS_CASES_AUTO_CALC_SINGLE)
+def test_forwards_cases_auto_calc_single(w, b, inp):
+    exp = _calc_auto_exp_forwards_single(w, b, inp)
+    return _run_test_forwards_single(w, b, inp, exp)
+
+
+@pytest.mark.parametrize(["w", "b", "inps"], _FORWARDS_CASES_AUTO_CALC_MULTI)
+def test_forwards_cases_auto_calc_multi(w, b, inps):
+    exps = _calc_auto_exp_forwards_multi(w, b, inps)
+    return _run_test_forwards_multi(w, b, inps, exps)
+
+
+@pytest.mark.parametrize(["w", "b", "xs", "grad_wrt_ys", "exp_ws_inc", "exp_bs_inc"], _BACKWARDS_CASES_CHANGE_DIRS_SINGLE)
+def test_backwards_cases_change_directions_single(
     w: npt.NDArray,
     b: npt.NDArray,
     xs: npt.NDArray,
@@ -275,7 +516,7 @@ def test_backwards_cases_change_directions(
     prev_bs = layer.bias.copy()
 
     # Act
-    layer.backwards(xs, grad_wrt_ys)
+    layer.backwards_single(xs, grad_wrt_ys)
 
     # Assert
 
@@ -289,8 +530,48 @@ def test_backwards_cases_change_directions(
     assert_allclose(bs_inc, exp_bs_inc)
 
 
-@pytest.mark.parametrize(["w", "b", "xs", "grad_wrt_ys", "exp"], _BACKWARDS_CASES_OUTPUT_GRADS)
-def test_backwards_cases_output_grads(
+@pytest.mark.parametrize(["w", "b", "xs", "grads_wrt_ys", "exp_ws_inc", "exp_bs_inc"], _BACKWARDS_CASES_CHANGE_DIRS_MULTI)
+def test_backwards_cases_change_directions_multi(
+    w: npt.NDArray,
+    b: npt.NDArray,
+    xs: npt.NDArray,
+    grads_wrt_ys: npt.NDArray,
+    exp_ws_inc: npt.NDArray[np.bool_],
+    exp_bs_inc: npt.NDArray[np.bool_]):
+
+    assert w.ndim == exp_ws_inc.ndim == 2
+    assert b.ndim == exp_bs_inc.ndim == 1
+    assert xs.ndim == grads_wrt_ys.ndim == 2
+    assert w.shape == exp_ws_inc.shape
+    assert b.shape == exp_bs_inc.shape
+    assert w.shape[0] == xs.shape[1]
+    assert w.shape[1] == b.shape[0] == grads_wrt_ys.shape[1]
+    assert xs.shape[0] == grads_wrt_ys.shape[0]
+
+    # Arrange
+
+    layer = _create(w, b)
+
+    prev_ws = layer.weights.copy()
+    prev_bs = layer.bias.copy()
+
+    # Act
+    layer.backwards_multi(xs, grads_wrt_ys)
+
+    # Assert
+
+    new_ws = layer.weights.copy()
+    new_bs = layer.bias.copy()
+
+    ws_inc = new_ws > prev_ws
+    bs_inc = new_bs > prev_bs
+
+    assert_allclose(ws_inc, exp_ws_inc)
+    assert_allclose(bs_inc, exp_bs_inc)
+
+
+@pytest.mark.parametrize(["w", "b", "xs", "grad_wrt_ys", "exp"], _BACKWARDS_CASES_OUTPUT_GRADS_SINGLE)
+def test_backwards_cases_output_grads_single(
     w: npt.NDArray,
     b: npt.NDArray,
     xs: npt.NDArray,
@@ -306,7 +587,32 @@ def test_backwards_cases_output_grads(
     layer = _create(w, b)
 
     # Act
-    out = layer.backwards(xs, grad_wrt_ys)
+    out = layer.backwards_single(xs, grad_wrt_ys)
+
+    # Assert
+    assert_allclose(out, exp)
+
+
+@pytest.mark.parametrize(["w", "b", "xs", "grad_wrt_ys", "exp"], _BACKWARDS_CASES_OUTPUT_GRADS_MULTI)
+def test_backwards_cases_output_grads_multi(
+    w: npt.NDArray,
+    b: npt.NDArray,
+    xs: npt.NDArray,
+    grad_wrt_ys: npt.NDArray,
+    exp: npt.NDArray):
+
+    assert w.ndim == 2
+    assert b.ndim == 1
+    assert xs.ndim == grad_wrt_ys.ndim == exp.ndim == 2
+    assert w.shape[0] == xs.shape[1] == exp.shape[1]
+    assert w.shape[1] == b.shape[0]
+    assert xs.shape[0] == grad_wrt_ys.shape[0] == exp.shape[0]
+
+    # Arrange
+    layer = _create(w, b)
+
+    # Act
+    out = layer.backwards_multi(xs, grad_wrt_ys)
 
     # Assert
     assert_allclose(out, exp)